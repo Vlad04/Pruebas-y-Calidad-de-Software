b340c03162dee76d4e579502bcd45c6f13c44121
Jose Ramon Garcia Gonzalez <ram00ngar@gmail.com>
2018-01-23 16:16:05 -0600

    calendar working

--- BeHealty/app/src/main/java/mx/itesm/behealty/MainActivity.java
+++ BeHealty/app/src/main/java/mx/itesm/behealty/MainActivity.java
@@ -1,13 +1,26 @@
 package mx.itesm.behealty;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button calendario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        calendario=findViewById(R.id.Calendario_Button);
+
+        calendario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Calendario.class);
+                startActivity(intent);
+            }
+        });
     }
 }
