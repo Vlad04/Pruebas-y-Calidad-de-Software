fb0d6319033c8aaf685c73de166af4da26a7efd8
Jose Ramon Garcia Gonzalez <ram00ngar@gmail.com>
2018-03-13 15:44:46 -0600

    testing changes by ramon computer

--- BeHealty/app/src/main/java/mx/itesm/behealty/FeedActivity.java
+++ BeHealty/app/src/main/java/mx/itesm/behealty/FeedActivity.java
@@ -9,5 +9,6 @@ public class FeedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        //Test from Ramon computer
     }
 }
